4e51ce62dc15e67427004dc45cb28a7da3553104
Andrew Forward <aforward@gmail.com>
2018-05-18 10:40:50 -0400

    Lab1 solutions

--- lab1/lib/lab1.ex
+++ lab1/lib/lab1.ex
@@ -1,17 +1,59 @@
 defmodule Lab1 do
   def print_first_message() do
-    raise "not implemented yet"
+    spawn(fn ->
+      receive do
+        message -> IO.write(:stderr, message)
+      end
+    end)
+  end
+
+  def loop() do
+    receive do
+      message ->
+        IO.write(:stderr, message)
+        loop()
+    end
   end
 
   def print_all_messages() do
-    raise "not implemented yet"
+    spawn(&loop/0)
   end
 
   def sum() do
-    raise "not implemented yet"
+    spawn(fn ->
+      receive do
+        {:sum, pid, list} ->
+          send(pid, Enum.sum(list))
+      end
+    end)
   end
 
-  def sum_all(_list_of_lists) do
-    raise "not implemented yet"
+  def sum_with_pid() do
+    spawn(fn ->
+      receive do
+        {:sum, pid, list} ->
+          send(pid, {self(), Enum.sum(list)})
+      end
+    end)
+  end
+
+  def sum_all(list_of_lists) do
+    me = self()
+
+    list_of_lists
+    |> Enum.map(fn list ->
+      ref = make_ref()
+
+      spawn(fn ->
+        send(me, {ref, Enum.sum(list)})
+      end)
+
+      ref
+    end)
+    |> Enum.map(fn ref ->
+      receive do
+        {^ref, sum} -> sum
+      end
+    end)
   end
 end
